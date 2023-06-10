a53df1b7ee643bc1f0d283fbd20719278e55aa4d
Aline Vieira <info.alinevieira@gmail.com>
2023-06-10 08:40:06 -0300

    adiciona entidade produto

---
+++ vendas-service/src/main/java/br/com/alinevieira/model/ProdutoModel.java
@@ -0,0 +1,37 @@
+package br.com.alinevieira.model;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produto")
+public class ProdutoModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	private String name;
+	private BigDecimal preco;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+}
