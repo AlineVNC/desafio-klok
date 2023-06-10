24bf6e59fcdde1d09adef50f093a1474b15e7ab5
Aline Vieira <info.alinevieira@gmail.com>
2023-06-10 08:41:36 -0300

    adiciona alguns produtos

---
+++ vendas-service/src/main/resources/db/migration/V004__adiciona-produtos.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_produto VALUES ('d29c88f9-b068-4701-89ca-c75fea5e38e1', 'Camisa Polo', 100.0, now());
+INSERT INTO tb_produto VALUES ('e864b83e-9ba9-4e4a-8ec3-5ee500a82507', 'Short', 70.0, now());
+INSERT INTO tb_produto VALUES ('793599c2-e8cd-4e9d-ad75-32e499cb2861', 'Vestido', 150.0, now());
+INSERT INTO tb_produto VALUES ('420ef8aa-0204-4294-9f05-3b8530557a69', 'Calça Jeans', 100.0, now());
+INSERT INTO tb_produto VALUES ('b75824a3-b6ef-48c6-aeb7-ca60fada9b5a', 'Blusa', 50.0, now());
+INSERT INTO tb_produto VALUES ('35a2048c-b426-46c7-8715-ffa04f31072d', 'Meia', 20.0, now());
