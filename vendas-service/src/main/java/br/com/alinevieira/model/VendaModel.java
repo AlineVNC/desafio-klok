5ea2e18af6b5d4aa3b0f619e2813d3b71522881c
Aline Vieira <info.alinevieira@gmail.com>
2023-06-10 08:36:07 -0300

    entidade venda

---
+++ vendas-service/src/main/java/br/com/alinevieira/model/VendaModel.java
@@ -0,0 +1,51 @@
+package br.com.alinevieira.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_venda")
+public class VendaModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	private LocalDateTime data;
+	
+	@OneToMany
+	private List<ProdutoModel> produtos;
+	
+	public UUID getId() {
+		return id;
+	}
+	
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	
+	public LocalDateTime getData() {
+		return data;
+	}
+	
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	public List<ProdutoModel> getProdutos() {
+		return produtos;
+	}
+	
+	public void setProdutos(List<ProdutoModel> produtos) {
+		this.produtos = produtos;
+	}
+}
---
+++ vendas-service/src/main/resources/db/migration/V001__cria-tabela-venda.sql
@@ -0,0 +1,4 @@
+CREATE TABLE tb_venda (
+    id uuid PRIMARY KEY,
+	data timestamp(0) NOT NULL
+);
