d71babc92fc9ca77e8aee4111ecedfd18567e796
Aline Vieira <info.alinevieira@gmail.com>
2023-06-10 21:41:29 -0300

    ajuste migration items

--- vendas-service/src/main/resources/db/migration/V005__adiciona_tabela_item.sql
+++ vendas-service/src/main/resources/db/migration/V005__adiciona_tabela_item.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tb_item (
 id uuid NOT NULL PRIMARY KEY,
-id_venda uuid REFERENCES tb_venda (id) ON DELETE CASCADE,
-id_produto uuid REFERENCES tb_produto (id) ON DELETE SET NULL,
-precoPraticado money NOT NULL,
+venda_id uuid REFERENCES tb_venda (id) ON DELETE CASCADE,
+produto_id uuid REFERENCES tb_produto (id) ON DELETE SET NULL,
+preco_praticado money NOT NULL,
 quantidade numeric NOT NULL DEFAULT 1
 );
