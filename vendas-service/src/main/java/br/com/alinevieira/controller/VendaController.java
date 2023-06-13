beff73fe4d2b7078522afa72b65b9ed42283db86
Aline Vieira <info.alinevieira@gmail.com>
2023-06-13 07:49:03 -0300

    Gets e delete

--- vendas-service/src/main/java/br/com/alinevieira/controller/VendaController.java
+++ vendas-service/src/main/java/br/com/alinevieira/controller/VendaController.java
@@ -1,7 +1,7 @@
 package br.com.alinevieira.controller;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -9,8 +9,12 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alinevieira.dtos.ItemDto;
@@ -25,6 +29,7 @@ import br.com.alinevieira.repository.VendaRepository;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping(path = "/vendas")
 public class VendaController {
 
 	@Autowired
@@ -36,13 +41,35 @@ public class VendaController {
 	@Autowired
 	ItemRepository itemRepository;
 	
-	public ResponseEntity<List<VendaModel>> listarVendas() {
+	@GetMapping
+	public ResponseEntity<List<VendaResponseDto>> listarVendas() {
 		List<VendaModel> vendas = vendaRepository.findAll();
-		ResponseEntity<List<VendaModel>> response = ResponseEntity.ok(vendas);
+		
+		List<VendaResponseDto> vendasResponse = new ArrayList<>();
+		for (VendaModel venda : vendas) {
+			VendaResponseDto vendaResponseDto = VendaResponseDto.fromModel(venda);
+			vendasResponse.add(vendaResponseDto);
+		}
+		
+		ResponseEntity<List<VendaResponseDto>> response = ResponseEntity.ok(vendasResponse);
 		return response; 
 	}
 	
-	@PostMapping("/vendas")
+	@GetMapping("/{id}")
+	public ResponseEntity<VendaResponseDto> pegarVendaPorId(@PathVariable(value="id") UUID id) {
+		Optional<VendaModel> optVenda = vendaRepository.findById(id);
+		if (optVenda.isPresent()) {
+			VendaModel venda = optVenda.get();
+			VendaResponseDto vendaResponseDto = VendaResponseDto.fromModel(venda);
+			ResponseEntity<VendaResponseDto> response = ResponseEntity.ok(vendaResponseDto);
+			return response;
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	
+	@PostMapping
 	public ResponseEntity<VendaResponseDto> criaVenda(@RequestBody @Valid VendaDto vendaDto) {
 		VendaModel venda = new VendaModel();
 		venda.setCpfComprador(vendaDto.cpfComprador());
@@ -79,5 +106,16 @@ public class VendaController {
 		VendaResponseDto vendaResponse = VendaResponseDto.fromModel(venda);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(vendaResponse);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deletarPorId(@PathVariable UUID id) {
+		boolean vendaExiste = vendaRepository.existsById(id);
+		if(vendaExiste) {
+			vendaRepository.deleteById(id);
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}	
 	}
 }
--- vendas-service/src/main/java/br/com/alinevieira/dtos/VendaDto.java
+++ vendas-service/src/main/java/br/com/alinevieira/dtos/VendaDto.java
@@ -1,6 +1,5 @@
 package br.com.alinevieira.dtos;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.validation.constraints.NotBlank;
@@ -8,9 +7,4 @@ import jakarta.validation.constraints.NotBlank;
 public record VendaDto(
 		@NotBlank String cpfComprador,
 		List<ItemDto> items
-		) { 
-//	public VendaDto {
-//		if (items == null) {
-//			items = new ArrayList<>();	
-//		}		
-	}
+		) { }
