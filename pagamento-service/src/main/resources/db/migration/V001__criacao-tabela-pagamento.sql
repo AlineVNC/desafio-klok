e8c719d8cfaee7b43ea4b9c5a1d663a614493547
Aline Vieira <info.alinevieira@gmail.com>
2023-06-16 20:30:03 -0300

    adicionado campos a tabela pagamento

--- pagamento-service/src/main/resources/db/migration/V001__criacao-tabela-pagamento.sql
+++ pagamento-service/src/main/resources/db/migration/V001__criacao-tabela-pagamento.sql
@@ -1,9 +1,11 @@
 CREATE TABLE tb_pagamento (
 	id uuid PRIMARY KEY,
+	venda_id uuid NOT NULL,
 	data_criacao timestamp(0) NOT NULL,
 	data_finalizacao timestamp(0),
 	valor decimal NOT NULL,
 	status varchar(20) NOT NULL,
+	rejeitado_por varchar(250),
 	tipo varchar(20) NOT NULL,
 	cpf_pagador varchar(14) NOT NULL
 );
